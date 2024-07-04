70f80af3fa6eb3d7ccfab604e26b4b1961ba40ef
Paul Gaudeaux <phvcgdx@gmail.com>
2024-07-04 15:58:56 +0200

    add haribo theme to actual conf

--- zsh/setup-zsh.sh
+++ zsh/setup-zsh.sh
@@ -1,6 +1,15 @@
 # === basic zsh === #
+cd zsh
+
 sudo apt install zsh
 chsh -s $(which zsh)
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+cp haribo.zsh-theme ~/.oh-my-zsh/custom/themes/
+
+cp .zshrc ~/.zshrc
+
+source ~/.zshrc
+
+cd -
