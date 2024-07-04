39a1809ffd83ce6fd1d0b8c106fb7a0449debd86
Paul Gaudeaux <phvcgdx@gmail.com>
2024-07-04 09:49:10 +0200

    initial-setup

--- init/initial-setup.sh
+++ init/initial-setup.sh
@@ -1,7 +1,30 @@
-# create the user if not created
+#!/bin/sh
+cd init
 
 # create the authors.txt file
+cp authors.txt ~/authors.txt
 
-# activate the root user
+# === activate the root user === #
+ROOT_PASSWORD=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 12)
+echo "root:$ROOT_PASSWORD" | sudo chpasswd
+echo "Root password: $ROOT_PASSWORD" > ~/root.txt
 
-# install docker
+# === install docker === #
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install docker
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+cd -
