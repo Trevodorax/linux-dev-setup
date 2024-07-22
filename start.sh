08a64cded39210b0c56ccec59c0e91196e5f2347
Paul Gaudeaux <phvcgdx@gmail.com>
2024-07-23 00:45:35 +0200

    add missing sudos to start script

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-chmod +x ./init/initial-setup.sh
-chmod +x ./zsh/setup-zsh.sh
-chmod +x ./ssh/setup-ssh.sh
-chmod +x ./sftp-server/setup-firewall.sh
-chmod +x ./sftp-server/setup-sftp.sh
-chmod +x ./services/php-web-app/cockpit.sh
-chmod +x ./services/docker.sh
-chmod +x ./android-env/setup-android-env.sh
+sudo chmod a+x ./init/initial-setup.sh
+sudo chmod a+x ./zsh/setup-zsh.sh
+sudo chmod a+x ./ssh/setup-ssh.sh
+sudo chmod a+x ./sftp-server/setup-firewall.sh
+sudo chmod a+x ./sftp-server/setup-sftp.sh
+sudo chmod a+x ./services/php-web-app/cockpit.sh
+sudo chmod a+x ./services/docker.sh
+sudo chmod a+x ./android-env/setup-android-env.sh
 
 ./init/initial-setup.sh
 
